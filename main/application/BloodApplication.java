6b446b904db9472b7665a0386e3a87feb7684e23
DHANALAKSHMI-08 <115217098+DHANALAKSHMI-08@users.noreply.github.com>
2024-04-24 11:51:12 +0530

    Add files via upload

---
+++ main/application/BloodApplication.java
@@ -0,0 +1,12 @@
+package com.cts.blood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class BloodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodApplication.class, args);
+	}
+
+}
